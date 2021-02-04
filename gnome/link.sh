5582bc8f1aa59eb0484b28dbf7a65fe8f0892b0c
Perry Werneck <perry.werneck@gmail.com>
2021-02-04 17:12:49 -0300

    Creating folder for extension related files.

---
+++ gnome/link.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+EXTENSION_ID="statusicons@werneck.eti.br"
+EXTENSION_FOLDER="${HOME}/.local/share/gnome-shell/extensions/${EXTENSION_ID}"
+
+ln -sf $(readlink -f ${EXTENSION_ID}) ${EXTENSION_FOLDER}
+
